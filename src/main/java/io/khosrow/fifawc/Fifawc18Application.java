dcf96be90d352d1ed21fe2f1f096530ae759d0b8
Khosrow Moossavi <khos2ow@gmail.com>
2018-06-08 00:17:38 -0400

    Initial commit

---
+++ src/main/java/io/khosrow/fifawc/Fifawc18Application.java
@@ -0,0 +1,12 @@
+package io.khosrow.fifawc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fifawc18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fifawc18Application.class, args);
+	}
+}
---
+++ src/test/java/io/khosrow/fifawc/Fifawc18ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.khosrow.fifawc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Fifawc18ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
